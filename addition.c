5953c9e2bebc9c5a66a6535d49723cdd3c89a2a5
anurag-dixit-lpu <anuragdixit.lpu@gmail.com>
2022-11-20 21:29:43 +0530

    My first commit

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main() {
+int a;
+int b;
+printf("please enter first number:- \n");
+scanf("%d", &a);
+printf("please enter second number:- \n");
+scanf("%d", &b);
+printf("your result is:- "); 
+printf("%d", a+b);
+
+}
